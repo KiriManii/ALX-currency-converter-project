6fa5cf3eb657f7a806de1750fd4fd534387d5ba3
Lewis Kimani <lkirigwi@gmail.com>
2025-01-11 14:16:35 +0300

    Create  App.jsx to use new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl text-center text-blue-500">Hello, Tailwind!</h1>
+    <div className="flex items-center justify-center h-screen bg-dark text-white">
+      <div className="w-full max-w-md p-6 bg-opacity-70 bg-gray-800 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center mb-6">
+          Lewis Kimani Currency Converter
+        </h2>
+        <ConverterForm />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
