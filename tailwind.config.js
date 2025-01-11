edd4244a7e8019242e03a33897f6e48e4838fbd0
Lewis Kimani <lkirigwi@gmail.com>
2025-01-11 14:17:47 +0300

    update tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensures Tailwind scans JS and JSX files
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1d4ed8", // Blue
+        secondary: "#ef4444", // Red
+        dark: "#000000", // Black
+      },
+    },
   },
   plugins: [],
 };
